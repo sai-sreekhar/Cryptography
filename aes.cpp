4f07179dad9b81249ddc184151745af44d1f9810
sai-sreekhar <sai.sreekhar@gmail.com>
2024-02-23 09:38:53 +0530

    fix: cleaned code

--- aes.cpp
+++ aes.cpp
@@ -230,61 +230,61 @@ void AddRoundKey(vector<unsigned char> &state, vector<unsigned char> &roundKey)
 
 void InverseMixColumns(vector<unsigned char> &state)
 {
-    vector<unsigned char> tmp(16);
+    vector<unsigned char> tempState(16);
 
-    tmp[0] = (unsigned char)mul14[state[0]] ^ mul11[state[1]] ^ mul13[state[2]] ^ mul9[state[3]];
-    tmp[1] = (unsigned char)mul9[state[0]] ^ mul14[state[1]] ^ mul11[state[2]] ^ mul13[state[3]];
-    tmp[2] = (unsigned char)mul13[state[0]] ^ mul9[state[1]] ^ mul14[state[2]] ^ mul11[state[3]];
-    tmp[3] = (unsigned char)mul11[state[0]] ^ mul13[state[1]] ^ mul9[state[2]] ^ mul14[state[3]];
+    tempState[0] = (unsigned char)mul14[state[0]] ^ mul11[state[1]] ^ mul13[state[2]] ^ mul9[state[3]];
+    tempState[1] = (unsigned char)mul9[state[0]] ^ mul14[state[1]] ^ mul11[state[2]] ^ mul13[state[3]];
+    tempState[2] = (unsigned char)mul13[state[0]] ^ mul9[state[1]] ^ mul14[state[2]] ^ mul11[state[3]];
+    tempState[3] = (unsigned char)mul11[state[0]] ^ mul13[state[1]] ^ mul9[state[2]] ^ mul14[state[3]];
 
-    tmp[4] = (unsigned char)mul14[state[4]] ^ mul11[state[5]] ^ mul13[state[6]] ^ mul9[state[7]];
-    tmp[5] = (unsigned char)mul9[state[4]] ^ mul14[state[5]] ^ mul11[state[6]] ^ mul13[state[7]];
-    tmp[6] = (unsigned char)mul13[state[4]] ^ mul9[state[5]] ^ mul14[state[6]] ^ mul11[state[7]];
-    tmp[7] = (unsigned char)mul11[state[4]] ^ mul13[state[5]] ^ mul9[state[6]] ^ mul14[state[7]];
+    tempState[4] = (unsigned char)mul14[state[4]] ^ mul11[state[5]] ^ mul13[state[6]] ^ mul9[state[7]];
+    tempState[5] = (unsigned char)mul9[state[4]] ^ mul14[state[5]] ^ mul11[state[6]] ^ mul13[state[7]];
+    tempState[6] = (unsigned char)mul13[state[4]] ^ mul9[state[5]] ^ mul14[state[6]] ^ mul11[state[7]];
+    tempState[7] = (unsigned char)mul11[state[4]] ^ mul13[state[5]] ^ mul9[state[6]] ^ mul14[state[7]];
 
-    tmp[8] = (unsigned char)mul14[state[8]] ^ mul11[state[9]] ^ mul13[state[10]] ^ mul9[state[11]];
-    tmp[9] = (unsigned char)mul9[state[8]] ^ mul14[state[9]] ^ mul11[state[10]] ^ mul13[state[11]];
-    tmp[10] = (unsigned char)mul13[state[8]] ^ mul9[state[9]] ^ mul14[state[10]] ^ mul11[state[11]];
-    tmp[11] = (unsigned char)mul11[state[8]] ^ mul13[state[9]] ^ mul9[state[10]] ^ mul14[state[11]];
+    tempState[8] = (unsigned char)mul14[state[8]] ^ mul11[state[9]] ^ mul13[state[10]] ^ mul9[state[11]];
+    tempState[9] = (unsigned char)mul9[state[8]] ^ mul14[state[9]] ^ mul11[state[10]] ^ mul13[state[11]];
+    tempState[10] = (unsigned char)mul13[state[8]] ^ mul9[state[9]] ^ mul14[state[10]] ^ mul11[state[11]];
+    tempState[11] = (unsigned char)mul11[state[8]] ^ mul13[state[9]] ^ mul9[state[10]] ^ mul14[state[11]];
 
-    tmp[12] = (unsigned char)mul14[state[12]] ^ mul11[state[13]] ^ mul13[state[14]] ^ mul9[state[15]];
-    tmp[13] = (unsigned char)mul9[state[12]] ^ mul14[state[13]] ^ mul11[state[14]] ^ mul13[state[15]];
-    tmp[14] = (unsigned char)mul13[state[12]] ^ mul9[state[13]] ^ mul14[state[14]] ^ mul11[state[15]];
-    tmp[15] = (unsigned char)mul11[state[12]] ^ mul13[state[13]] ^ mul9[state[14]] ^ mul14[state[15]];
+    tempState[12] = (unsigned char)mul14[state[12]] ^ mul11[state[13]] ^ mul13[state[14]] ^ mul9[state[15]];
+    tempState[13] = (unsigned char)mul9[state[12]] ^ mul14[state[13]] ^ mul11[state[14]] ^ mul13[state[15]];
+    tempState[14] = (unsigned char)mul13[state[12]] ^ mul9[state[13]] ^ mul14[state[14]] ^ mul11[state[15]];
+    tempState[15] = (unsigned char)mul11[state[12]] ^ mul13[state[13]] ^ mul9[state[14]] ^ mul14[state[15]];
 
     for (int i = 0; i < 16; i++)
     {
-        state[i] = tmp[i];
+        state[i] = tempState[i];
     }
 }
 
 void inverseShiftRows(vector<unsigned char> &state)
 {
-    vector<unsigned char> tmp(16);
+    vector<unsigned char> tempState(16);
 
-    tmp[0] = state[0];
-    tmp[1] = state[13];
-    tmp[2] = state[10];
-    tmp[3] = state[7];
+    tempState[0] = state[0];
+    tempState[1] = state[13];
+    tempState[2] = state[10];
+    tempState[3] = state[7];
 
-    tmp[4] = state[4];
-    tmp[5] = state[1];
-    tmp[6] = state[14];
-    tmp[7] = state[11];
+    tempState[4] = state[4];
+    tempState[5] = state[1];
+    tempState[6] = state[14];
+    tempState[7] = state[11];
 
-    tmp[8] = state[8];
-    tmp[9] = state[5];
-    tmp[10] = state[2];
-    tmp[11] = state[15];
+    tempState[8] = state[8];
+    tempState[9] = state[5];
+    tempState[10] = state[2];
+    tempState[11] = state[15];
 
-    tmp[12] = state[12];
-    tmp[13] = state[9];
-    tmp[14] = state[6];
-    tmp[15] = state[3];
+    tempState[12] = state[12];
+    tempState[13] = state[9];
+    tempState[14] = state[6];
+    tempState[15] = state[3];
 
     for (int i = 0; i < 16; i++)
     {
-        state[i] = tmp[i];
+        state[i] = tempState[i];
     }
 }
 
@@ -321,61 +321,61 @@ void SubBytes(vector<unsigned char> &state)
 
 void ShiftRows(vector<unsigned char> &state)
 {
-    vector<unsigned char> tmp(16);
+    vector<unsigned char> tempState(16);
 
-    tmp[0] = state[0];
-    tmp[1] = state[5];
-    tmp[2] = state[10];
-    tmp[3] = state[15];
+    tempState[0] = state[0];
+    tempState[1] = state[5];
+    tempState[2] = state[10];
+    tempState[3] = state[15];
 
-    tmp[4] = state[4];
-    tmp[5] = state[9];
-    tmp[6] = state[14];
-    tmp[7] = state[3];
+    tempState[4] = state[4];
+    tempState[5] = state[9];
+    tempState[6] = state[14];
+    tempState[7] = state[3];
 
-    tmp[8] = state[8];
-    tmp[9] = state[13];
-    tmp[10] = state[2];
-    tmp[11] = state[7];
+    tempState[8] = state[8];
+    tempState[9] = state[13];
+    tempState[10] = state[2];
+    tempState[11] = state[7];
 
-    tmp[12] = state[12];
-    tmp[13] = state[1];
-    tmp[14] = state[6];
-    tmp[15] = state[11];
+    tempState[12] = state[12];
+    tempState[13] = state[1];
+    tempState[14] = state[6];
+    tempState[15] = state[11];
 
     for (int i = 0; i < 16; i++)
     {
-        state[i] = tmp[i];
+        state[i] = tempState[i];
     }
 }
 
 void MixColumns(vector<unsigned char> &state)
 {
-    vector<unsigned char> tmp(16);
+    vector<unsigned char> tempState(16);
 
-    tmp[0] = (unsigned char)mul2[state[0]] ^ mul3[state[1]] ^ state[2] ^ state[3];
-    tmp[1] = (unsigned char)state[0] ^ mul2[state[1]] ^ mul3[state[2]] ^ state[3];
-    tmp[2] = (unsigned char)state[0] ^ state[1] ^ mul2[state[2]] ^ mul3[state[3]];
-    tmp[3] = (unsigned char)mul3[state[0]] ^ state[1] ^ state[2] ^ mul2[state[3]];
+    tempState[0] = (unsigned char)mul2[state[0]] ^ mul3[state[1]] ^ state[2] ^ state[3];
+    tempState[1] = (unsigned char)state[0] ^ mul2[state[1]] ^ mul3[state[2]] ^ state[3];
+    tempState[2] = (unsigned char)state[0] ^ state[1] ^ mul2[state[2]] ^ mul3[state[3]];
+    tempState[3] = (unsigned char)mul3[state[0]] ^ state[1] ^ state[2] ^ mul2[state[3]];
 
-    tmp[4] = (unsigned char)mul2[state[4]] ^ mul3[state[5]] ^ state[6] ^ state[7];
-    tmp[5] = (unsigned char)state[4] ^ mul2[state[5]] ^ mul3[state[6]] ^ state[7];
-    tmp[6] = (unsigned char)state[4] ^ state[5] ^ mul2[state[6]] ^ mul3[state[7]];
-    tmp[7] = (unsigned char)mul3[state[4]] ^ state[5] ^ state[6] ^ mul2[state[7]];
+    tempState[4] = (unsigned char)mul2[state[4]] ^ mul3[state[5]] ^ state[6] ^ state[7];
+    tempState[5] = (unsigned char)state[4] ^ mul2[state[5]] ^ mul3[state[6]] ^ state[7];
+    tempState[6] = (unsigned char)state[4] ^ state[5] ^ mul2[state[6]] ^ mul3[state[7]];
+    tempState[7] = (unsigned char)mul3[state[4]] ^ state[5] ^ state[6] ^ mul2[state[7]];
 
-    tmp[8] = (unsigned char)mul2[state[8]] ^ mul3[state[9]] ^ state[10] ^ state[11];
-    tmp[9] = (unsigned char)state[8] ^ mul2[state[9]] ^ mul3[state[10]] ^ state[11];
-    tmp[10] = (unsigned char)state[8] ^ state[9] ^ mul2[state[10]] ^ mul3[state[11]];
-    tmp[11] = (unsigned char)mul3[state[8]] ^ state[9] ^ state[10] ^ mul2[state[11]];
+    tempState[8] = (unsigned char)mul2[state[8]] ^ mul3[state[9]] ^ state[10] ^ state[11];
+    tempState[9] = (unsigned char)state[8] ^ mul2[state[9]] ^ mul3[state[10]] ^ state[11];
+    tempState[10] = (unsigned char)state[8] ^ state[9] ^ mul2[state[10]] ^ mul3[state[11]];
+    tempState[11] = (unsigned char)mul3[state[8]] ^ state[9] ^ state[10] ^ mul2[state[11]];
 
-    tmp[12] = (unsigned char)mul2[state[12]] ^ mul3[state[13]] ^ state[14] ^ state[15];
-    tmp[13] = (unsigned char)state[12] ^ mul2[state[13]] ^ mul3[state[14]] ^ state[15];
-    tmp[14] = (unsigned char)state[12] ^ state[13] ^ mul2[state[14]] ^ mul3[state[15]];
-    tmp[15] = (unsigned char)mul3[state[12]] ^ state[13] ^ state[14] ^ mul2[state[15]];
+    tempState[12] = (unsigned char)mul2[state[12]] ^ mul3[state[13]] ^ state[14] ^ state[15];
+    tempState[13] = (unsigned char)state[12] ^ mul2[state[13]] ^ mul3[state[14]] ^ state[15];
+    tempState[14] = (unsigned char)state[12] ^ state[13] ^ mul2[state[14]] ^ mul3[state[15]];
+    tempState[15] = (unsigned char)mul3[state[12]] ^ state[13] ^ state[14] ^ mul2[state[15]];
 
     for (int i = 0; i < 16; i++)
     {
-        state[i] = tmp[i];
+        state[i] = tempState[i];
     }
 }
 
